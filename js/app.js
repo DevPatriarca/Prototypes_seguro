ebee74d49dad96520bd720568611a40625c682d8
Juan Jiménez <nefioxdev@gmail.com>
2021-02-10 21:52:39 -0600

    Agregando validación al formulario

--- js/app.js
+++ js/app.js
@@ -32,3 +32,24 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con años
 })
 
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    const marca = document.querySelector('#marca').value;
+    
+    const year = document.querySelector('#year').value;
+	
+	// Validación
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    if(marca === '' || tipo === '') {
+        console.log('No valida');
+    } else {
+        console.log('Sí valida');
+    }
+}
